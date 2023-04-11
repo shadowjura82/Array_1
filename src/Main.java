4f0e15f282b79e8a7c12e6e7788614fbdf9aefcf
Yurii <shadowjura82@gmail.com>
2023-04-11 07:19:07 +0200

    task1 has been completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+    }
 
+    public static void task1() {
+        byte[] byteMass = new byte[]{1, 2, 3};
+        float[] floatMas = {1.57f, 7.654f, 9.986f};
+        boolean[] flagsSet = new boolean[23];
     }
 }
