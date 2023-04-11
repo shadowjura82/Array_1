6ce4019088a27dfde2629eb9ccefc159719e9f6b
Yurii <shadowjura82@gmail.com>
2023-04-11 08:57:40 +0200

    task4 has been completed

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,16 @@
 public class Main {
+    static byte[] byteMass = new byte[]{1, 2, 3};
+    static float[] floatMas = {1.57f, 7.654f, 9.986f};
+    static char[] charMas = {'М', 'и', 'р', '!'};
+
     public static void main(String[] args) {
-        task1();
         task2();
         task3();
-    }
-
-    public static void task1() {
-        byte[] byteMass = new byte[]{1, 2, 3};
-        float[] floatMas = {1.57f, 7.654f, 9.986f};
-        boolean[] flagsSet = new boolean[23];
+        task4();
     }
 
     public static void task2() {
         System.out.println("Задача 2");
-        byte[] byteMass = new byte[]{1, 2, 3};
-        float[] floatMas = {1.57f, 7.654f, 9.986f};
-        char[] charMas = {'М', 'и', 'р', '!'};
         for (int i = 0; i < byteMass.length; i++) {
             if (i != byteMass.length - 1) {
                 System.out.print(byteMass[i] + ", ");
@@ -33,13 +28,11 @@ public class Main {
                 System.out.print(charMas[i] + ", ");
             } else System.out.print(charMas[i]);
         }
+        System.out.println("\n");
     }
 
     public static void task3() {
         System.out.println("Задача 3");
-        byte[] byteMass = new byte[]{1, 2, 3};
-        float[] floatMas = {1.57f, 7.654f, 9.986f};
-        char[] charMas = {'М', 'и', 'р', '!'};
         for (int i = byteMass.length - 1; i >= 0; i--) {
             if (i != 0) {
                 System.out.print(byteMass[i] + ", ");
@@ -57,5 +50,13 @@ public class Main {
                 System.out.print(charMas[i] + ", ");
             } else System.out.print(charMas[i]);
         }
+        System.out.println("\n");
+    }
+
+    public static void task4() {
+        System.out.println("Задача 4");
+        for (int i = 0; i < byteMass.length; i++)
+            if (byteMass[i] % 2 != 0) byteMass[i]++;
+        for (byte x : byteMass) System.out.print(x + " ");
     }
 }
